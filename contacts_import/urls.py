532dabcf7bbe9e5743e0b098384530d728bb555e
Patrick Altman <paltman@gmail.com>
2010-11-17 00:43:48 -0600

    comment out oauth_access url addition as when it is included in parent project there is a conflict

--- contacts_import/urls.py
+++ contacts_import/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns("",
     url(r"^facebook_auth/$", "contacts_import.views.facebook_auth", name="import_facebook_auth"),
     url(r"^twitter_auth/$", "contacts_import.views.twitter_auth", name="import_twitter_auth"),
     url(r"^authsub/login/$", "contacts_import.views.authsub_login", name="authsub_login"),
-    url(r"^oauth_access/", include("oauth_access.urls")),
+    # url(r"^oauth_access/", include("oauth_access.urls")),
 )
