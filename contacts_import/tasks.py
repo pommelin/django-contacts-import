5141485233a0f4e36c4f9d1edfc9612e8a200b41
pommelin <pommelin6fr@gmail.com>
2011-07-19 10:58:28 +0200

    remove s in the tasks

--- contacts_import/tasks.py
+++ contacts_import/tasks.py
@@ -3,6 +3,6 @@ from celery.task import task
 from contacts_import.backends import importers
 
 
-tasks.register(importers.VcardImporter)
-tasks.register(importers.GoogleImporter)
-tasks.register(importers.YahooImporter)
+task.register(importers.VcardImporter)
+task.register(importers.GoogleImporter)
+task.register(importers.YahooImporter)
